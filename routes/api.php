617c1c38e9bb4ac0eb68094cb75b23d9f7ea78b2
Kelvin Murumba <kevmurumba@Kelvins-MacBook-Pro.local>
2019-04-20 14:45:11 +0300

    Complete Need to be tested

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('articles', 'ArticleController@index');
+Route::group(['middleware' => 'auth:api'], function (){
+    Route::get('articles', 'ArticleController@index');
 
-Route::get('articles/{articles}', 'ArticleController@show');
+    Route::get('articles/{articles}', 'ArticleController@show');
 
-Route::post('articles', 'ArticleController@store');
+    Route::post('articles', 'ArticleController@store');
 
-Route::put('articles/{articles}', 'ArticleController@update');
+    Route::put('articles/{articles}', 'ArticleController@update');
 
-Route::delete('articles/{articles}', 'ArticleController@delete');
+    Route::delete('articles/{articles}', 'ArticleController@delete');
+});
 
+Route::post('register', 'Auth\RegisterController@register');
 
+Route::post('login', 'Auth\LoginController@login');
+
+Route::post('logout', 'Auth\LoginController@logout');
 
